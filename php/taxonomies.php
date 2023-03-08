79bf94176f1f6a0c262cbcbba0ab463f4203bb0b
Jan Macario <jmacario@gmu.edu>
2023-03-08 00:33:25 -0500

    Adds website custom post type taxonomy for content management systems.

--- php/taxonomies.php
+++ php/taxonomies.php
@@ -64,4 +64,31 @@ add_action('init', function(){
 			)
 		);
 
+	// Register cms taxonomy
+		register_taxonomy(
+			'cms',
+			'website',
+			array(
+			'hierarchical' => false,
+			'labels' => array(
+				'name' => 'CMS(s)',
+				'singular_name' => 'Content Management System',
+				'search_items' =>  'Search Content Management Systems',
+				'all_items' => 'All Content Management Systems',
+				'parent_item' => 'Parent Content Management System',
+				'parent_item_colon' => 'Parent Content Management System:',
+				'edit_item' => 'Edit Content Management System',
+				'update_item' => 'Update Content Management System',
+				'add_new_item' => 'Add New Content Management System',
+				'new_item_name' => 'New Content Management System',
+				'menu_name' => 'Content Management Systems',
+				),
+			'show_ui' => true,
+			'query_var' => true,
+			'rewrite' => array( 'slug' => 'web_host' ),
+			'show_admin_column' => true,
+			'show_in_rest' => true
+			)
+		);
+
 });
