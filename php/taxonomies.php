6255da8ccae85a60b27927a3fb79a93adb3262d2
Jan Macario <jmacario@gmu.edu>
2023-03-08 00:25:32 -0500

    Adds website custom post type taxonomy for web hosts.

--- php/taxonomies.php
+++ php/taxonomies.php
@@ -37,4 +37,31 @@ add_action('init', function(){
 			)
 		);
 
+	// Register web host taxonomy
+		register_taxonomy(
+			'web_host',
+			'website',
+			array(
+			'hierarchical' => true,
+			'labels' => array(
+				'name' => 'Web Host(s)',
+				'singular_name' => 'Web Host',
+				'search_items' =>  'Search Web Hosts',
+				'all_items' => 'All Web Hosts',
+				'parent_item' => 'Parent Web Host',
+				'parent_item_colon' => 'Parent Web Host:',
+				'edit_item' => 'Edit Web Host',
+				'update_item' => 'Update Web Host',
+				'add_new_item' => 'Add New Web Host',
+				'new_item_name' => 'New Web Host',
+				'menu_name' => 'Web Hosts',
+				),
+			'show_ui' => true,
+			'query_var' => true,
+			'rewrite' => array( 'slug' => 'web_host' ),
+			'show_admin_column' => true,
+			'show_in_rest' => true
+			)
+		);
+
 });
