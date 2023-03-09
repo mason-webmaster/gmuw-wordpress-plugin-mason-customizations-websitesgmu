068bcc5ba03bd56a10c44e45d401a45b82489ef1
Jan Macario <jmacario@gmu.edu>
2023-03-08 22:35:01 -0500

    Registers new custom post type for GTM accounts.

--- php/post-types.php
+++ php/post-types.php
@@ -159,4 +159,54 @@ add_action('init', function(){
     // Register ga_property custom post type
     register_post_type('ga_property', $args);
 
+
+    // Register gtm_account custom post type.
+
+    // Define labels for gtm_account post type
+    $labels = array(
+        'name'                  => 'GTM Account(s)',
+        'singular_name'         => 'GTM Account',
+        'menu_name'             => 'GTM Accounts',
+        'name_admin_bar'        => 'GTM Account',
+        'add_new'               => 'Add New',
+        'add_new_item'          => 'Add New GTM Account',
+        'new_item'              => 'New GTM Account',
+        'edit_item'             => 'Edit GTM Account',
+        'view_item'             => 'View GTM Account',
+        'all_items'             => 'All GTM Accounts',
+        'search_items'          => 'Search GTM accounts',
+        'parent_item_colon'     => 'Parent GTM account:',
+        'not_found'             => 'No GTM accounts found.',
+        'not_found_in_trash'    => 'No GTM accounts found in Trash.',
+        'featured_image'        => 'GTM Account Image',
+        'set_featured_image'    => 'Set GTM account image',
+        'remove_featured_image' => 'Remove GTM account image',
+        'use_featured_image'    => 'Use as GTM account image',
+        'archives'              => 'GTM Accounts archives',
+        'insert_into_item'      => 'Insert into GTM account',
+        'uploaded_to_this_item' => 'Uploaded to this GTM account',
+        'filter_items_list'     => 'Filter GTM account list',
+        'items_list_navigation' => 'GTM account list navigation',
+        'items_list'            => 'GTM account list',
+    );
+
+    // Set up arguments for the register_post_type function
+    $args = array(
+        'labels'            => $labels,
+        'public'            => true,
+        'show_ui'           => true,
+        'query_var'         => true,
+        'rewrite'           => array('slug' => 'gtm_account'),
+        'capability_type'   => 'post',
+        'has_archive'       => true,
+        'hierarchical'      => false,
+        'menu_position'     => 20,
+        'menu_icon'         => 'dashicons-category',
+        'show_in_rest'      => true,
+        'supports'          => array('title', 'editor', 'thumbnail'),
+    );
+
+    // Register gtm_account custom post type
+    register_post_type('gtm_account', $args);
+
 });
