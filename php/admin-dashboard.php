078cf4a13cc9460ca4e71b2ce39b125db5715a19
Jan Macario <jmacario@gmu.edu>
2023-03-14 01:25:20 -0400

    Adds custom 'links' dashboard meta box.

--- php/admin-dashboard.php
+++ php/admin-dashboard.php
@@ -15,6 +15,10 @@ function gmuw_websitesgmu_custom_dashboard_meta_boxes() {
   // Declare global variables
   global $wp_meta_boxes;
 
+  /* Add 'links' meta box */
+  add_meta_box("gmuw_websitesgmu_custom_dashboard_meta_box_links", "Links", "gmuw_websitesgmu_custom_dashboard_meta_box_links", "dashboard","normal");
+
+
   /* Add 'follow-up' meta box */
   add_meta_box("gmuw_websitesgmu_custom_dashboard_meta_box_follow_up", "Follow-up Records", "gmuw_websitesgmu_custom_dashboard_meta_box_follow_up", "dashboard","normal");
 
@@ -27,5 +31,29 @@ function gmuw_websitesgmu_custom_dashboard_meta_box_follow_up() {
 
   //Output links to follow-up records
   echo gmuw_websitesgmu_display_follow_up_records();
+
+}
+
+/**
+ * Provides content for the dashboard 'links' meta box
+ */
+function gmuw_websitesgmu_custom_dashboard_meta_box_links() {
+
+  //Output content
+
+  echo '<h3>Tools</h3>';
+  echo '<p><a href="https://toolbox.googleapps.com/apps/dig/" target="_blank">Google Dig</a></p>';
+
+  echo '<h3>Web Hosting</h3>';
+  echo '<p><a href="https://dashboard.sucuri.net/" target="_blank">Sucuri</a></p>';
+  echo '<p><a href="https://my.wpengine.com/" target="_blank">WPEngine</a></p>';
+  echo '<p><a href="https://cloud.materiell.com/login" target="_blank">Materiell Cloud</a></p>';
+  echo '<p><a href="https://materiell.zendesk.com/hc/en-us/restricted?return_to=https%3A%2F%2Fmateriell.zendesk.com%2Fhc%2Fen-us" target="_blank">Materiell Support</a></p>';
+  echo '<p><a href="https://www.georgemasonusf.acsitefactory.com/" target="_blank">Acquia Cloud SiteFactory</a></p>';
+
+  echo '<h3>Mason ITS</h3>';
+  echo '<p><a href="https://its.gmu.edu/service/web-hosting/" target="_blank">ITS Website Web Hosting Service Page</a></p>';
+  echo '<p><a href="https://gmu.teamdynamix.com/TDNext/Home/Desktop/Default.aspx" target="_blank">TeamDynamix</a></p>';
+
 
 }
