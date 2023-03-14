4b51845b7d5652c7021846025277bd96fb7f401a
Jan Macario <jmacario@gmu.edu>
2023-03-14 01:29:05 -0400

    Adds additional Google links to custom 'links' dashboard meta box.

--- php/admin-dashboard.php
+++ php/admin-dashboard.php
@@ -44,6 +44,11 @@ function gmuw_websitesgmu_custom_dashboard_meta_box_links() {
   echo '<h3>Tools</h3>';
   echo '<p><a href="https://toolbox.googleapps.com/apps/dig/" target="_blank">Google Dig</a></p>';
 
+  echo '<h3>Google</h3>';
+  echo '<p><a href="https://analytics.google.com/" target="_blank">Google Analytics</a></p>';
+  echo '<p><a href="https://tagmanager.google.com/" target="_blank">Google Tag Manager</a></p>';
+  echo '<p><a href="https://search.google.com/search-console" target="_blank">Google Search Console</a></p>';
+
   echo '<h3>Web Hosting</h3>';
   echo '<p><a href="https://dashboard.sucuri.net/" target="_blank">Sucuri</a></p>';
   echo '<p><a href="https://my.wpengine.com/" target="_blank">WPEngine</a></p>';
@@ -54,6 +59,5 @@ function gmuw_websitesgmu_custom_dashboard_meta_box_links() {
   echo '<h3>Mason ITS</h3>';
   echo '<p><a href="https://its.gmu.edu/service/web-hosting/" target="_blank">ITS Website Web Hosting Service Page</a></p>';
   echo '<p><a href="https://gmu.teamdynamix.com/TDNext/Home/Desktop/Default.aspx" target="_blank">TeamDynamix</a></p>';
-
 
 }
