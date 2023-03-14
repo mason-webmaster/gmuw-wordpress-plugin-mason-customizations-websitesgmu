26aae865d6fb8a62eb1eeffa4d94273527208147
Jan Macario <jmacario@gmu.edu>
2023-03-13 22:12:19 -0400

    Moves functions relating to real-time checking of website theme version to more appropriate php file.

--- php/post-type-website-functions.php
+++ php/post-type-website-functions.php
@@ -268,3 +268,59 @@ function gmuw_websitesgmu_custom_dashboard_meta_box_websites_statistics_content(
   echo gmuw_websitesgmu_websites_content_statistics();
 
 }
+
+function gmuw_websitesgmu_get_live_website_theme($post_id){
+  //Get the real-time theme info from the WP REST API on the live site
+
+      // Get post object
+      $post = get_post($post_id);
+      //$return_value.='Post ID: '.$post_id.'<br />';
+
+      // Get post hosting domain=
+      $domain = gmuw_websitesgmu_hosting_domain(wp_get_post_terms($post_id,'web_host')[0]->slug, get_post_meta($post_id, 'environment_name', true),false);
+      //$return_value.='Domain: '.$domain.'<br />';
+
+      // Set URL for REST endpoint
+      $mason_site_check_in_theme_info_endpoint_url='https://' . $domain . '/wp-json/gmuj-sci/theme-info';
+      //$return_value.='URL: '.$mason_site_check_in_theme_info_endpoint_url.'<br />';
+
+      // Try to get the info
+      $mason_site_check_in_theme_info_response = gmuw_msc_get_url_content($mason_site_check_in_theme_info_endpoint_url);
+        //$return_value.='<p><textarea>'.$mason_site_check_in_theme_info_response.'</textarea></p>';
+      // Try to parse it as JSON
+      $mason_site_check_in_theme_info_response_json = json_decode($mason_site_check_in_theme_info_response);
+        //$return_value.=var_dump($mason_site_check_in_theme_info_response_json);
+
+        // Did we get a json response?
+        if (gettype($mason_site_check_in_theme_info_response_json)!='object'){
+          $return_value.='<p><a href="'.$mason_site_check_in_theme_info_endpoint_url.'" target="_blank">Not JSON object.</a> &#128533;</p>';
+        } else {
+          if ($mason_site_check_in_theme_info_response_json->data->status==404){
+              $return_value.='<p><a href="'.$mason_site_check_in_theme_info_endpoint_url.'" target="_blank">JSON 404.</a> &#128533;</p>';
+          } else {
+            $return_value.='<p>';
+            $return_value.='<a href="'.$mason_site_check_in_theme_info_endpoint_url.'" target="_blank">'.$mason_site_check_in_theme_info_response_json->theme_name.' ('.$mason_site_check_in_theme_info_response_json->theme_version.')</a> &#128515;';
+            $return_value.='</p>';
+
+            // Is the post meta theme name different from the live theme name?
+            if (get_post_meta($post_id, 'wordpress_theme', true ) != $mason_site_check_in_theme_info_response_json->theme_name.' ('.$mason_site_check_in_theme_info_response_json->theme_version.')') {
+              $return_value.='<p>';
+              $return_value.=gmuw_websitesgmu_update_theme_link($post_id,$mason_site_check_in_theme_info_response_json->theme_name,$mason_site_check_in_theme_info_response_json->theme_version);
+              $return_value.='</p>';
+            }
+
+          }
+        }
+
+      // Return value
+      return $return_value;
+
+}
+
+function gmuw_websitesgmu_update_theme_link($post_id,$theme_name,$theme_version){
+  //Display theme update link
+
+  //Return value
+  return "<a class='button button-small' href='admin.php?page=gmuw_websitesgmu&action=update_theme&post_id=". $post_id ."&theme_name=". urlencode($theme_name) ."&theme_version=". $theme_version ."'>Update Post</a>";
+
+}
