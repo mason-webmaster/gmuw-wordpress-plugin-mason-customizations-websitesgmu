87ebf8febe8416d9c178fe1ac7c001c13db1601c
Jan Macario <jmacario@gmu.edu>
2023-06-05 16:37:50 -0400

    Separates generation of GA property and GTM container admin links into two functions: generating the URL and generating the full HTML code.

--- php/post-type-ga_property-functions.php
+++ php/post-type-ga_property-functions.php
@@ -5,7 +5,23 @@
 
 
 /**
- * Gets URL of ag property admin link given post id
+ * Gets URL for GA property admin link given ga_property post id
+ */
+function gmuw_websitesgmu_ga_property_admin_link_url($post_id) {
+
+	// Initialize variables
+	$return_value='';
+
+	// build link
+	$return_value.='https://analytics.google.com/analytics/web/#/a'.get_post_meta(get_post_meta($post_id, 'ga_property_account_post_id', true), 'ga_account_id', true).'p'.get_post_meta($post_id, 'ga_property_id', true).'/admin';
+
+	// Return value
+	return $return_value;
+
+}
+
+/**
+ * Gets HTML for GA property admin link given ga_property post id
  */
 function gmuw_websitesgmu_ga_property_admin_link($post_id) {
 
@@ -13,7 +29,7 @@ function gmuw_websitesgmu_ga_property_admin_link($post_id) {
 	$return_value='';
 
 	// build link
-	echo '<a href="https://analytics.google.com/analytics/web/#/a'.get_post_meta(get_post_meta($post_id, 'ga_property_account_post_id', true), 'ga_account_id', true).'p'.get_post_meta($post_id, 'ga_property_id', true).'/admin" target="_blank"><img style="width:25px; vertical-align: middle; margin-bottom:1px;" src="'.plugin_dir_url( __DIR__ ).'images/logo-google_analytics.png'.'" /></a><br />';
+	echo '<a href="'.gmuw_websitesgmu_ga_property_admin_link_url($post_id).'" target="_blank"><img class="gmuw_websitesgmu_offsitelink gmuw_websitesgmu_ga_property_link" style="width:25px; vertical-align: middle; margin-bottom:1px;" src="'.plugin_dir_url( __DIR__ ).'images/logo-google_analytics.png'.'" /></a><br />';
 
 	// Return value
 	return $return_value;
--- php/post-type-gtm_container-functions.php
+++ php/post-type-gtm_container-functions.php
@@ -5,6 +5,22 @@
 
 
 /**
+ * Gets URL for GTM container admin link given gtm_container post id
+ */
+function gmuw_websitesgmu_gtm_container_admin_link_url($post_id) {
+
+	// Initialize variables
+	$return_value='';
+
+	// build link
+	$return_value.='https://tagmanager.google.com/#/container/accounts/'.get_post_meta(get_post_meta($post_id, 'gtm_container_account_post_id', true), 'gtm_account_id', true).'/containers/'.get_post_meta($post_id, 'gtm_container_id', true).'/';
+
+	// Return value
+	return $return_value;
+
+}
+
+/**
  * Gets URL of gtm container admin link given post id
  */
 function gmuw_websitesgmu_gtm_container_admin_link($post_id) {
@@ -13,7 +29,7 @@ function gmuw_websitesgmu_gtm_container_admin_link($post_id) {
 	$return_value='';
 
 	// build link
-  $return_value= '<a href="https://tagmanager.google.com/#/container/accounts/'.get_post_meta(get_post_meta($post_id, 'gtm_container_account_post_id', true), 'gtm_account_id', true).'/containers/'.get_post_meta($post_id, 'gtm_container_id', true).'/" target="_blank"><img style="width:25px; vertical-align: middle; margin-bottom:1px;" src="'.plugin_dir_url( __DIR__ ).'images/logo-google_tag_manager.png'.'" /></a><br />';
+  $return_value= '<a href="'.gmuw_websitesgmu_gtm_container_admin_link_url($post_id).'" target="_blank"><img class="gmuw_websitesgmu_offsitelink gmuw_websitesgmu_gtm_container_link" style="width:25px; vertical-align: middle; margin-bottom:1px;" src="'.plugin_dir_url( __DIR__ ).'images/logo-google_tag_manager.png'.'" /></a><br />';
 
 	// Return value
 	return $return_value;
